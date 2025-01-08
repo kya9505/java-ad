67e83a4c09f9abf4d369166f7daf44abed9b713a
kya9505 <dbsdk4211@naver.com>
2025-01-08 16:53:01 +0900

    "rerere commit"

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+
+        for(int i=1; i <=100; i++){
+            System.out.println("i =" + i);
+
+          if( i % 2 == 0){
+            sum += i;
+          }
+        }
+        System.out.println("1부터 100까지의 짝수 합은 : " + sum);
+    } //왜안돼
+}
+
---
+++ ex1.java
@@ -0,0 +1,14 @@
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("첫 번째 프로그램입니다.");
+        System.out.println("--------------------");
+        System.out.println("두 번째 프로그램입니다. 한 줄에 여러 문장을 출력합니다.");
+        System.out.println("--------------------");
+        System.out.println("세 번째 프로그램입니다.");
+        System.out.println("줄을 바꾸어 출력합니다.");
+        System.out.println("여기까지 출력하고");
+        System.out.println("줄을 바꿉니다.");
+        System.out.println("--------------------");
+        System.out.println("서식 문자로 출력하는 방법입니다.\n이것은 문자열을 나타냅니다.");
+    }
+}
