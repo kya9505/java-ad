dd0fc06f6a1674d696d2293921b4e7df2e4551e4
kya9505 <dbsdk4211@naver.com>
2025-01-09 15:10:56 +0900

    "1504commit"

---
+++ day02/Var1.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class Var1 {
+    public static void main(String[] args) {
+        /* 정수형 변수 value 선언
+        value 변수에 10을 할당
+        value 변수에 사용자에게 숫자를 입력받아 해당 숫자를 더한 후 result변수에 저장
+        result 출력하는 addSum클래스를 작성하여 결과를 확인하세요.
+         */
+
+    }
+}
