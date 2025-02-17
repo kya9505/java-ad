a0d064bdaa589cc110f2f3848aff3a5c802faafc
kya <dbsdk4211@naver.com>
2025-02-18 08:02:35 +0900

    1111

--- src/day13/coputer/Circlecal.java
+++
@@ -1,9 +0,0 @@
-package day13.coputer;
-
-public class Circlecal {
-
-    public double areaCircle(double radius){
-        System.out.println("나는 서클의 메소드야");
-        return 3.14 *radius *radius;
-    }
-}
